b2e39a16300ce7f596fbf2cb38644687affdf0e7
Kayc <chauchuti15@gmail.com>
2024-10-16 10:43:29 -0300

    first commit

--- Migrations/20241016134049_InitialCreate.Designer.cs
+++ Migrations/20241016134049_InitialCreate.Designer.cs
@@ -11,7 +11,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Nicolas.Migrations
 {
     [DbContext(typeof(AppDataContext))]
-    [Migration("20241016133221_InitialCreate")]
+    [Migration("20241016134049_InitialCreate")]
     partial class InitialCreate
     {
         /// <inheritdoc />
